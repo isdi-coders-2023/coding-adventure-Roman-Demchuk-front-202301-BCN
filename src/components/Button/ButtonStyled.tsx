29aa5945a57bce8f2ee9760c90e96b64ed5d1527
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-01 15:39:16 +0100

    Erase id and htmlFor from Form, make button responsive

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -6,18 +6,23 @@ const ButtonStyled = styled.button`
   color: var(--primaryColor);
   border: none;
   border-radius: 10px;
-  width: 323.02px;
   height: 40px;
   font-family: var(--displayFont);
   font-size: 1.25rem;
   align-self: center;
+  width: 320px;
 
   :disabled {
     opacity: 57%;
   }
 
+  @media (max-width: 360px) {
+    width: 100%;
+  }
+
   @media (min-width: 987px) {
-    height: 57px;
+    width: 320px;
+    height: 60px;
   }
 `;
 
